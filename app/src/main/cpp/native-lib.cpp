365b37eb69edeb31da01cc3810f32fb61f0ebe44
黄文杰 <huangwj@huaweisoft.com>
2021-04-16 21:10:55 +0800

    实现native层播放声音

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,40 @@
-#include <jni.h>
-#include <string>
+#include <unistd.h>
+#include "com_androidlittleboy_fmodpractice_MainActivity.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_androidlittleboy_fmodpractice_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C"
+JNIEXPORT void JNICALL Java_com_androidlittleboy_fmodpractice_MainActivity_voiceChangeNative
+        (JNIEnv *env, jobject jobject1, jint jint1, jstring jstring1) {
+    char *content_ = "默认 播放完毕";
+    const char *path = env->GetStringUTFChars(jstring1, NULL);
+    //音效引擎系统
+    FMOD::System * system1 = 0;
+    //声音
+    FMOD::Sound * sound = 0;
+    //音轨
+    FMOD::Channel * channel = 0;
+    //数据信号处理
+    FMOD::DSP * dsp = 0;
+
+    //创建系统
+    FMOD::System_Create(&system1);
+    //系统的初始化
+    system1->init(32,FMOD_INIT_NORMAL,0);
+    //创建声音
+    system1->createSound(path,FMOD_DEFAULT,0,&sound);
+    //播放声音
+    system1->playSound(sound,0, false,&channel);
+
+    bool isPlaying = true;
+    while (isPlaying){
+        channel->isPlaying(&isPlaying);
+        usleep(1000*1000);
+    }
+
+    //回收
+    sound->release();
+    system1->close();
+    system1->release();
+    env->ReleaseStringUTFChars(jstring1,path);
+
+
 }
--- app/src/main/java/com/androidlittleboy/fmodpractice/MainActivity.java
+++ app/src/main/java/com/androidlittleboy/fmodpractice/MainActivity.java
@@ -3,14 +3,27 @@ package com.androidlittleboy.fmodpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import org.fmod.FMOD;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MODE_NORMAL = 0;
+    private static final int MODE_LUOLI = 1;
+    private static final int MODE_DASHU = 2;
+    private static final int MODE_JINGSONG = 3;
+    private static final int MODE_GAOGUAI = 4;
+    private static final int MODE_KONGLING = 5;
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +31,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        path = "file:///android_asset/derry.mp3";
+        FMOD.init(this);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        FMOD.close();
+    }
+
+
+    public void onFix(View view) {
+        switch (view.getId()) {
+            case R.id.btn_normal:
+                voiceChangeNative(MODE_NORMAL, path); // 真实开发中，必须子线程  JNI线程（很多坑）
+                break;
+            case R.id.btn_luoli:
+                voiceChangeNative(MODE_LUOLI, path);
+                break;
+            case R.id.btn_dashu:
+                voiceChangeNative(MODE_DASHU, path);
+                break;
+            case R.id.btn_jingsong:
+                voiceChangeNative(MODE_JINGSONG, path);
+                break;
+            case R.id.btn_gaoguai:
+                voiceChangeNative(MODE_GAOGUAI, path);
+                break;
+            case R.id.btn_kongling:
+                voiceChangeNative(MODE_KONGLING, path);
+                break;
+        }
+    }
+
+    private native void voiceChangeNative(int modeNormal, String path);
 }
